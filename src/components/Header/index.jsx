97b64f3ae976f44220d419357aa6d662d5bda776
serhiilaptop <sergebrew@gmail.com>
2023-04-24 17:01:47 -0600

    layout, routes, starting redux-store

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+import Button from "@mui/material/Button";
+
+import styles from "./Header.module.scss";
+import Container from "@mui/material/Container";
+
+export const Header = () => {
+  const isAuth = false;
+
+  const onClickLogout = () => {};
+
+  return (
+    <div className={styles.root}>
+      <Container maxWidth="lg">
+        <div className={styles.inner}>
+          <Link className={styles.logo} to="/">
+            <div>PitWithMagic BLOG</div>
+          </Link>
+          <div className={styles.buttons}>
+            {isAuth ? (
+              <>
+                <Link to="/posts/create">
+                  <Button variant="contained">Write a post</Button>
+                </Link>
+                <Button
+                  onClick={onClickLogout}
+                  variant="contained"
+                  color="error"
+                >
+                  Log out
+                </Button>
+              </>
+            ) : (
+              <>
+                <Link to="/login">
+                  <Button variant="outlined">Sign in</Button>
+                </Link>
+                <Link to="/register">
+                  <Button variant="contained">Sign up</Button>
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
