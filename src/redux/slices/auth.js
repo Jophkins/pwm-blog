6fbaa0411cd7a6fe8f4b8235420e24fa5f86fc32
serhiilaptop <sergebrew@gmail.com>
2023-05-03 17:38:46 -0600

    login authorization token to local storage. signIn signOut logic

--- src/redux/slices/auth.js
+++ src/redux/slices/auth.js
@@ -10,6 +10,11 @@ export const fetchAuth = createAsyncThunk(
   }
 );
 
+export const fetchAuthMe = createAsyncThunk("auth/fetchAuthMe", async () => {
+  const { data } = await axios.get("/auth/me");
+  return data;
+});
+
 const initialState = {
   data: null,
   status: "loading",
@@ -33,6 +38,18 @@ const authSlice = createSlice({
       state.data = action.payload;
     },
     [fetchAuth.rejected]: (state) => {
+      state.status = "error";
+      state.data = null;
+    },
+    [fetchAuthMe.pending]: (state) => {
+      state.status = "loading";
+      state.data = null;
+    },
+    [fetchAuthMe.fulfilled]: (state, action) => {
+      state.status = "loaded";
+      state.data = action.payload;
+    },
+    [fetchAuthMe.rejected]: (state) => {
       state.status = "error";
       state.data = null;
     },
